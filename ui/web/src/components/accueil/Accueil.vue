6667483fae189cd300f2e9265168c63ce7816794
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2022-01-12 14:39:45 +0100

    ajout des cartes des offres d'emploies a l'acceuil

--- ui/web/src/components/accueil/Accueil.vue
+++ ui/web/src/components/accueil/Accueil.vue
@@ -12,6 +12,57 @@
             <CardBid/>
         </div>
         </div>
+        <div class="div_offres_recentes">
+            <div class="lign_offres">
+            </div>
+            <div class="img_offres_recentes">
+                <img src='../../assets/offresRecentes.png'/>
+            </div>
+            <div class="lign_offres">
+            </div>
+        </div>
+         <div class="flex_acceuil">
+            <div>
+                <CardBid1/>
+            </div>
+            <div>
+                <CardBid1/>
+            </div>
+            <div>
+                <CardBid1/>
+            </div>
+        </div>
+        <div class="div_offres_recentes">
+            <div class="lign_offres">
+            </div>
+            <div class="img_offres_recentes">
+                <img src='../../assets/offresCandidates.png'/>
+            </div>
+            <div class="lign_offres">
+            </div>
+        </div>
+          <div class="flex_acceuil">
+            <div>
+                <CardBid1/>
+            </div>
+            <div>
+                <CardBid1/>
+            </div>
+            <div>
+                <CardBid1/>
+            </div>
+        </div>
+          <div class="flex_acceuil">
+            <div>
+                <CardBid1/>
+            </div>
+            <div>
+                <CardBid1/>
+            </div>
+            <div>
+                <CardBid1/>
+            </div>
+        </div>
     </div>
 </template>
 <style src="./accueil.css">
--- ui/web/src/components/accueil/accueil.js
+++ ui/web/src/components/accueil/accueil.js
@@ -1,11 +1,13 @@
 import TNavBar from './tNavBar/TNavBar.vue'
 import MyProfile from './myProfile/MyProfile.vue'
 import CardBid from './cardBid/CardBid.vue'
+import CardBid1 from './cardBid1/CardBid1.vue'
 export default {
   name: 'accueil',
   components: {
     TNavBar,
     MyProfile,
-    CardBid
+    CardBid,
+    CardBid1
   }
 }
