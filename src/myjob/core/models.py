8623a2e3c70d7333ebf5ba7173c167d137982b83
armand <tabantsazembougarmand@gmail.com>
2022-01-20 17:46:56 +0100

    Ajout des fonctions retournant les dix derniers demande d'emploi avec leus details

--- src/myjob/core/models.py
+++ src/myjob/core/models.py
@@ -18,8 +18,18 @@ class Metier(models.Model):
     class Meta:
         abstract=False
 
+class Travail(models.Model):
+    travail_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self) :
+        return "{}".format(self.travail_text)
 
-
+class Choix(models.Model):
+    travail = models.ForeignKey(Travail, on_delete=models.CASCADE)
+    choix_text = models.CharField(max_length=200)
+    nbre_place = models.IntegerField(default=0)
+    def __str__(self):
+       return "{}".format(self.choix_text)
 class ProfilUser(Profil):
     """
     Description: Model Description
